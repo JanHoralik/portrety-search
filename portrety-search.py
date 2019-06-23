b23adb0586dceeeb7b88e94a82dc50f8b5aecd75
Jan Horalik <horalik78@gmail.com>
2019-06-23 11:51:02 +0200

    Gets list of all Portrety items, both formats, with download URLs

--- portrety-search.py
+++ portrety-search.py
@@ -1,41 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
 
+PAGES_START = 0
 PAGES = 100
 
 output = []
-for page in range(0,PAGES):
+errors = []
+for page in range(PAGES_START,PAGES):
 
     offset = str(10*page)
     url = 'https://hledani.rozhlas.cz/iRadio/?query=&reader=&porad%5B%5D=Portr%C3%A9ty&offset='+offset
     resp = requests.get(url, verify=False)
-    #print(resp.text)
 
     b = BeautifulSoup(resp.text)
     uls = b.findAll('ul', {'class': 'box-audio-archive'})
     item = 0
     for ul in uls:
-        #print(ul)
         downloadUrl = ''
         try:    
             download = ul.find('div', class_='action action-download')
-            #print(download)
             downloadUrl = download.a['href']
         except:
-            print('!!! Cannot parse href of offset: {0}, item: {1}'.format(offset, item))
-        if not downloadUrl.startswith('https://plus.rozhlas.cz'):
+            errors.append('!!! Cannot parse href of offset: {0}, item: {1}, content: {2}'.format(offset, item, download))
             continue
-        try:
+
+        if downloadUrl.startswith('https://plus.rozhlas.cz'):
             title = ul.findAll('img')[0]['title']
-            #print(title)
-            #print(downloadUrl)
             output.append('{0}, {1}'.format(title,downloadUrl))
-        except:
-            print('!!! Cannot parse title of offset: {0}, item: {1}'.format(offset, item))
+        elif downloadUrl.startswith('http://media.rozhlas.cz'):
+            title = ul.find('div',class_='title').text
+            output.append('{0}, {1}'.format(title,downloadUrl)) 
+        else:
+            print('!!! Unknown format of download URL, offset {0}, item {1}'.format(offset, item))
+            print(ul)
         item +=1
 
 print(output)
 print(len(output))
+
+print(errors)
+print(len(errors))
+
 
         
 
